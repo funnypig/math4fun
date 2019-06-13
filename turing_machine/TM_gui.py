70323ec50c27d66b057a4b6e45f06a8d6d708d2f
ilya antipiev <funnypig1606@gmail.com>
2019-06-14 02:27:18 +0300

    turing nachine update

--- turing_machine/TM_gui.py
+++ turing_machine/TM_gui.py
@@ -13,7 +13,9 @@ from PyQt5.QtGui import QIcon, QFont, QColor
 from PyQt5.QtCore import pyqtSlot, QEvent, pyqtSignal, QBasicTimer
 import PyQt5.QtWidgets
 
+import clipboard
 import time
+import pickle
 
 import TuringMachine
 import sys
@@ -22,6 +24,7 @@ import sys
 DIGITS = [chr(i) for i in range(ord('0'), ord('9')+1)]
 DIGITS.extend([chr(i) for i in range(ord('a'), ord('z')+1)])
 
+SPEED = 50
 
 class MachineGUI(QWidget):
 
@@ -53,14 +56,25 @@ class MachineGUI(QWidget):
         #---------------------------------------------------------------------------------
         # First line
         self.Line = QTableWidget()
-        self.grid.addWidget(self.Line, 0, 0, 1, 10)
+        self.grid.addWidget(self.Line, 0, 0, 2, 9)
         self.drawLine()
+
+        self.SaveLineButton = QPushButton("Copy")
+        self.PasteLineButton = QPushButton("Paste")
+
+        self.SaveLineButton.clicked.connect(self.SaveLine)
+        self.PasteLineButton.clicked.connect(self.PasteLine)
+        self.SaveLineButton.setFont(self.font)
+        self.PasteLineButton.setFont(self.font)
+
+        self.grid.addWidget(self.PasteLineButton, 0, 10)
+        self.grid.addWidget(self.SaveLineButton, 1, 10)
 
         #---------------------------------------------------------------------------------
         # Second line
         label = QLabel('Instructions:')
         label.setFont(self.font)
-        self.grid.addWidget(label, 1, 0)
+        self.grid.addWidget(label, 2, 0)
 
         #---------------------------------------------------------------------------------
         # Third line
@@ -71,7 +85,7 @@ class MachineGUI(QWidget):
         self.instructionTable.setVerticalHeaderLabels(self.machine.digits)
         self.instructionTable.setHorizontalHeaderLabels(['q1'])
 
-        self.grid.addWidget(self.instructionTable, 2, 0, 3, 9)
+        self.grid.addWidget(self.instructionTable, 3, 0, 4, 9)
 
         #---------------------------------------------------------------------------------
         # Fourth line
@@ -83,9 +97,24 @@ class MachineGUI(QWidget):
         self.addDigitButton.setFont(self.font)
         self.addDigitButton.clicked.connect(self.addDigit)
 
-        self.grid.addWidget(self.addStateButton, 2, 10)
-        self.grid.addWidget(self.addDigitButton, 3, 10)
+        self.grid.addWidget(self.addStateButton, 3, 10)
+        self.grid.addWidget(self.addDigitButton, 4, 10)
 
+
+        self.importMachineButton = QPushButton("Import")
+        self.importMachineButton.setFont(self.font)
+        self.importMachineButton.clicked.connect(self.importMachine)
+
+        self.saveMachineButton = QPushButton("Save")
+        self.saveMachineButton.setFont(self.font)
+        self.saveMachineButton.clicked.connect(self.saveMachine)
+
+        self.grid.addWidget(self.saveMachineButton, 5, 10)
+        self.grid.addWidget(self.importMachineButton, 6, 10)
+
+
+        #
+        self.grid.addWidget(QLabel(""), 8, 0)
 
         #---------------------------------------------------------------------------------
         # Fifth line
@@ -101,25 +130,40 @@ class MachineGUI(QWidget):
         self.applyCurPosButton.clicked.connect(self.applyCurPos)
         self.applyLineButton.clicked.connect(self.applyLine)
 
-        self.grid.addWidget(self.applyInstructionButton, 6, 0)
-        self.grid.addWidget(self.applyCurPosButton, 6, 1)
-        self.grid.addWidget(self.applyLineButton, 6, 2)
+        self.grid.addWidget(self.applyInstructionButton, 9, 0)
+        self.grid.addWidget(self.applyCurPosButton, 9, 1)
+        self.grid.addWidget(self.applyLineButton, 9, 2)
 
 
         #---------------------------------------------------------------------------------
         # Sixth line
         playfont = QFont()
         playfont.setBold(True)
-        playfont.setPixelSize(30)
+        playfont.setPixelSize(22)
 
         self.PlayButton = QPushButton("PLAY", self)
         self.PlayButton.setFont(playfont)
         self.PlayButton.clicked.connect(self.play)
 
-        self.grid.addWidget(self.PlayButton, 5, 10, 2, 1)
+        self.grid.addWidget(self.PlayButton, 9, 10)
+
+        self.Stop = True
+        self.StopButton = QPushButton("STOP", self)
+        self.StopButton.setFont(playfont)
+        self.StopButton.clicked.connect(self.setStop)
+
+        self.grid.addWidget(self.StopButton, 10, 10)
+
+        # Speed box
+        sp = QLabel("Speed (1-100):")
+        sp.setFont(self.font)
+        self.grid.addWidget(sp, 10, 0)
+        self.speedBox = QLineEdit("50")
+        self.grid.addWidget(self.speedBox,10, 1)
+        self.speedBox.textChanged.connect(lambda s: self.setSpeed(s))
 
         #---------------------------------------------------------------------------------
-        self.grid.setRowStretch(0,5)
+        #self.grid.setRowStretch(0,6)
         self.grid.activate()
         self.show()
 
@@ -147,7 +191,64 @@ class MachineGUI(QWidget):
         self.Line.setHorizontalHeaderLabels(headers)
         self.Line.resizeRowsToContents()
         self.Line.resizeColumnsToContents()
-        self.grid.addWidget(self.Line, 0, 0, 1, 10)
+        self.grid.addWidget(self.Line, 0, 0, 2, 9)
+
+    @pyqtSlot()
+    def PasteLine(self):
+        s = clipboard.paste()
+        line = []
+        for si in s:
+            if si in self.machine.digits:
+                line.append(si)
+        self.machine.setLine(line)
+        self.drawLine()
+
+    @pyqtSlot()
+    def SaveLine(self):
+        clipboard.copy(''.join(self.machine.line))
+
+    @pyqtSlot()
+    def setSpeed(self, s):
+        global SPEED
+
+        try:
+            sp = int(s)
+            if sp>=100: sp = 100
+            if sp<1: sp = 1
+
+            SPEED = 1050-sp*10
+
+            self.speedBox.setStyleSheet("background-color: rgb(255,255,255);")
+        except:
+            self.speedBox.setStyleSheet("background-color: rgb(244, 66, 146);")
+
+    @pyqtSlot()
+    def importMachine(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fname, _ = QFileDialog.getOpenFileName(self, "Save Turing Machine", "", "Machine (*.pickle)", options=options)
+
+        file = open(fname, 'rb')
+        TM = pickle.loads(file.read())
+        file.close()
+
+        copyline = self.machine.line.copy()
+        self.machine = TM
+        self.machine.setLine(copyline)
+
+        self.instructionTable.clear()
+
+    def saveMachine(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fname, _ = QFileDialog.getSaveFileName(self, "Save Turing Machine", "", "Machine (*.pickle)", options=options)
+
+        with open(fname, 'wb') as file:
+            file.write(pickle.dumps(self.machine))
+
+    @pyqtSlot()
+    def setStop(self):
+        self.Stop = True
 
     @pyqtSlot()
     def addState(self):
@@ -227,50 +328,53 @@ class MachineGUI(QWidget):
 
     def timerEvent(self, QTimerEvent):
 
-        if self.step > 50:
+        try:
+            if self.Stop:
+                raise StopIteration
 
             try:
-                try:
-                    playState = next(self.playMachine)
-                    newDigit, moveTo, newState = playState
-                except KeyError:
-                    msg = QMessageBox.about(self, "Message", "Machine failed!")
-                    self.timer.stop()
-                    return
-
-                if newState!=0:
-                    for i in range(self.instructionTable.rowCount()):
-
-                        for j in range(self.instructionTable.columnCount()):
-                            try:
-                                self.instructionTable.item(i, j).setBackground(QColor(255, 255, 255))
-                            except: pass
-                    #print(DIGITS.index(newDigit), newState-1)
-
-                    try:
-                        self.instructionTable.item(DIGITS.index(newDigit), newState-1).setBackground(QColor(171, 255, 130))
-                    except: pass
-
-                self.drawLine()
-
-
-
-                self.step = 0
-                self.timer.start(0, self)
-
-            except StopIteration:
+                playState = next(self.playMachine)
+                newDigit, moveTo, newState = playState
+            except KeyError:
+                msg = QMessageBox.about(self, "Message", "Machine failed!")
                 self.timer.stop()
-
-                msg = QMessageBox.about(self, "Message", "Machine has finished its work!")
                 return
-        else:
-            time.sleep(0.01)
-            self.step += 1
+            if newState!=0:
+                for i in range(self.instructionTable.rowCount()):
+                    for j in range(self.instructionTable.columnCount()):
+                        try:
+                            self.instructionTable.item(i, j).setBackground(QColor(255, 255, 255))
+                        except: pass
+                #print(DIGITS.index(newDigit), newState-1)
+                try:
+                    self.instructionTable.item(DIGITS.index(newDigit), newState-1).setBackground(QColor(171, 255, 130))
+                except: pass
+
+            self.drawLine()
+            self.step = 0
+            self.timer.start(SPEED, self)
+
+        except StopIteration:
+            self.timer.stop()
+
+            # clean colors
+            for i in range(self.instructionTable.rowCount()):
+                for j in range(self.instructionTable.columnCount()):
+                    try:
+                        self.instructionTable.item(i, j).setBackground(QColor(255, 255, 255))
+                    except:
+                        pass
+
+
+            msg = QMessageBox.about(self, "Message", "Machine has finished its work!")
+            return
+
 
 
     @pyqtSlot()
     def play(self):
 
+        self.Stop = False
         self.timer = QBasicTimer()
 
         self.playMachine = self.machine.play()
